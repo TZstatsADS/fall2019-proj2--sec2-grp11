ad2216fe07477cd267dc4a2f48d70b3b55314cb0
Marshall Zhao <zhaomarshall@gmail.com>
2019-10-08 15:44:34 -0400

    added zipcode areas

--- app/server.R
+++ app/server.R
@@ -10,16 +10,58 @@ library(dplyr)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
+  # data preparation 
+  
+  # From https://data.cityofnewyork.us/Business/Zip-Code-Boundaries/i8iw-xf4u/data
+  NYCzipcodes <- readOGR("../data/ZIP_CODE_040114.shp",
+                         #layer = "ZIP_CODE", 
+                         verbose = FALSE)
+  df <- read.csv('../data/sqs_pk_fy18_final.csv')
+  
+  selZip <- subset(NYCzipcodes, NYCzipcodes$ZIPCODE %in% as.character(df$Zipcode))
+  
+  
+  # ----- Transform to EPSG 4326 - WGS84 (required)
+  subdat<-spTransform(selZip, CRS("+init=epsg:4326"))
+  
+  # ----- save the data slot
+  subdat_data=subdat@data[,c("ZIPCODE", "POPULATION")]
+  subdat.rownames=rownames(subdat_data)
+  #  subdat_data=
+  #    subdat_data%>%left_join(count.df, by=c("ZIPCODE" = "region"))
+  rownames(subdat_data)=subdat.rownames
+  
+  # ----- to write to geojson we need a SpatialPolygonsDataFrame
+  subdat<-SpatialPolygonsDataFrame(subdat, data=subdat_data)
+  
+  # ----- set uo color pallette https://rstudio.github.io/leaflet/colors.html
+  # Create a continuous palette function
+  pal <- colorNumeric(
+    palette = "Blues",
+    domain = subdat$POPULATION
+  )
+  
 
+  
+  
+  
+  
+    # simple map
     output$map <- renderLeaflet({
-      
-      leaflet() %>%
+      leaflet(zipcode) %>%
         addProviderTiles(providers$Stamen.TonerLite,
                          options = providerTileOptions(noWrap = TRUE)) %>%
-        setView(-73.983,40.7639,zoom = 12) 
-    
-
-
+        setView(-73.983,40.7639,zoom = 12) %>% 
+        addPolygons( data = subdat,
+          stroke = T, weight=1,
+          fillOpacity = 0.6,
+          color = ~pal(POPULATION)
+        )
+      
+      
+      
+  
+      
   })
-    
 })
+
